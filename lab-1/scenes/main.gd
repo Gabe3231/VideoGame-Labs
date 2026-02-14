1a7781c0a9676f194eb36822e87968177b6a52be
Gabriel <dearaujo.c.gabriel@gmail.com>
2026-02-14 17:35:33 -0500

    Lab 1 Complete

---
+++ lab-1/scenes/main.gd
@@ -0,0 +1,40 @@
+extends Node
+
+#setting up variables for later implmentation
+var game_running : bool
+var game_over : bool
+var scroll
+var score
+const scroll_speed : int = 4
+var screen_size : Vector2i
+var ground_height : int
+var pipes : Array
+const pipe_delay : int = 100
+const pipe_range : int = 200
+
+func _ready():
+	new_game()
+
+func new_game():
+	game_running = false
+	game_over = false
+	score = 0
+	scroll = 0
+	$ship.reset()
+	
+func _input(event):
+	if game_over == false:
+		# usinf mouse for now may transition to ekybinds
+		if event is InputEventMouseButton:
+			if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+				if game_running == false:
+					start_game()
+				else:
+					if $ship.flying:
+						$ship.flap()
+
+func start_game():
+	game_running = true
+	$ship.flying = true
+	$ship.flap()
+	
---
+++ lab-1/scenes/ship.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+const Gravity: int = 1000
+const MAX_VEL: int = 600
+const flap_speed: int = -500
+# need to do more tetsing with vector
+const start_pos = Vector2(200, 500)
+
+var flying: bool = false
+var falling: bool = false
+
+func _ready():
+	reset()
+
+func reset():
+	falling = false
+	flying = false
+	velocity = Vector2.ZERO
+	position = start_pos
+	set_rotation(0)
+
+func _physics_process(delta):
+	if flying or falling:
+		velocity.y += Gravity * delta
+		if velocity.y > MAX_VEL:
+			velocity.y = MAX_VEL
+		if flying:
+			set_rotation(deg_to_rad(velocity.y * 0.05))
+			$AnimatedSprite2D.play()
+		elif falling:
+			set_rotation(PI / 2)
+			$AnimatedSprite2D.stop()
+		move_and_slide()
+	else:
+		$AnimatedSprite2D.stop()
+
+func flap():
+	velocity.y = flap_speed
